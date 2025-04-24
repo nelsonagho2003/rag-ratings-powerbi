898956e4e140afb27b6147fd2f2c309b14cf6f2e
nelsonagho2003 <131207896+nelsonagho2003@users.noreply.github.com>
2025-04-24 15:24:59 +0100

    Add files via upload

---
+++ Beehive.sql
@@ -0,0 +1,54 @@
+-- Grouping the total number of student by tutor, status, and timestamp 
+WITH cte AS (
+    SELECT
+        c1.WBT AS work_based_tutor,
+        c1.COND_RAG AS condition_rag,
+        COUNT(*) AS student_count,
+        c1.SNAPSHOT_PERIOD
+    FROM USER_TAB_OWNER.UT_PEOPLE_BS_CASELOAD_RAG AS c1
+    LEFT OUTER JOIN (
+        SELECT DISTINCT 
+            WBT, 
+            COND_RAG 
+        FROM USER_TAB_OWNER.UT_PEOPLE_BS_CASELOAD_RAG
+    ) AS c2 ON c1.WBT = c2.WBT AND c1.COND_RAG = c2.COND_RAG
+    WHERE
+        c1.WBT NOT IN ('Maria Tomkinson', 'Billy Gilchrist', 'Gavin Smith', 'Terence O''Brien')
+        AND c1.STATUS NOT IN ('Achiever', 'Completed', 'Withdrawn') 
+    GROUP BY
+        c1.WBT, 
+        c1.COND_RAG, 
+        c1.SNAPSHOT_PERIOD
+)
+
+SELECT 
+    *,
+    LAG(student_count, 1) OVER (
+        PARTITION BY work_based_tutor, condition_rag
+        ORDER BY CONVERT(DATE, '01 ' + SNAPSHOT_PERIOD, 113)
+    ) AS student_count_previous_month
+FROM cte
+ORDER BY work_based_tutor, condition_rag, CONVERT(DATE, '01 ' + SNAPSHOT_PERIOD, 113);
+-- Grouping the total number of students by apprenticeship standard, status and timestamp
+SELECT
+	REPLACE(c1.APPRENTICESHIP_STANDARD, 'Apprenticeship Standard - ', '') AS apprenticeship_standard,
+	c1.COND_RAG AS condition_rag,
+	COUNT(*) AS student_count,
+	c1.SNAPSHOT_PERIOD AS monthyear_name
+FROM
+	USER_TAB_OWNER.UT_PEOPLE_BS_CASELOAD_RAG AS c1
+	LEFT OUTER JOIN (
+	SELECT DISTINCT
+		APPRENTICESHIP_STANDARD,
+		COND_RAG
+	FROM
+		USER_TAB_OWNER.UT_PEOPLE_BS_CASELOAD_RAG
+	) AS c2 ON c1.APPRENTICESHIP_STANDARD = c2.APPRENTICESHIP_STANDARD 
+	AND c1.COND_RAG = c2.COND_RAG
+WHERE
+	c1.WBT NOT IN ('Maria Tomkinson', 'Billy Gilchrist', 'Gavin Smith', 'Terence O''Brien')
+	AND c1.STATUS NOT IN ('Achievers', 'Completed', 'Withdrawn')
+GROUP BY
+	c1.APPRENTICESHIP_STANDARD,
+	c1.COND_RAG,
+	c1.SNAPSHOT_PERIOD
